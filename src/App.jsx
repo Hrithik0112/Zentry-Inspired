00e1c98328e4b580da09b48c21f985e78a98287e
Hrithik <slumdog4321@gmail.com>
2024-11-22 23:17:41 +0530

    Feat: define fonts and color configs. 💥

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='font-bold text-3xl text-purple-600 text-center'>Welcome to Zentry Inspiration</h1> 
+      <h1 className='font-bold text-3xl text-purple-600 text-center font-circular-web'>Welcome to Zentry Inspiration</h1> 
     </>
   )
 }
