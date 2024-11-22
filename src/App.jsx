4dcf27e30bace8141f946ca63e7ee29aa6db72cc
Hrithik <slumdog4321@gmail.com>
2024-11-22 23:10:22 +0530

    Initialized vite with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-3xl text-purple-600 text-center'>Welcome to Zentry Inspiration</h1> 
+    </>
+  )
+}
+
+export default App
