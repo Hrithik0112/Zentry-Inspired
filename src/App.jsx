6d9a50e844b57d7225cc180779e60108fc2b6822
Hrithik <slumdog4321@gmail.com>
2024-11-23 01:30:32 +0530

    Feat: Animated About section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "./components/Hero";
+import About from "./components/About";
 function App() {
   return (
     <main className="min-h-screen w-screen overflow-x-hidden relative">
       <Hero />
+      <About/>
     </main>
   );
 }
