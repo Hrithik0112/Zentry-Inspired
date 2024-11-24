703698bf89fbe94ee3b7bcfdee828d7c9935dd4e
Hrithik <slumdog4321@gmail.com>
2024-11-24 16:38:30 +0530

    Feat : Added bento grid feature section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 function App() {
   return (
-    <main className="min-h-screen w-screen overflow-x-hidden relative bg-zinc-600">
+    <main className="min-h-screen w-screen overflow-x-hidden relative">
       <Navbar />
       <Hero />
       <About/>
+      <Features/>
     </main>
   );
 }
